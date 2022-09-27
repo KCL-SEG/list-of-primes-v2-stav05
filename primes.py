894ac9f519c4f8e841110fe4e3e8d02e6e76ed8d
Stav Armoni-Friedmann <stav.armoni-friedmann@kcl.ac.uk>
2022-09-27 18:34:02 +0100

    add prime number finder

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError()
+
+
     list = []
+    counter = 1;
+    while len(list) < number_of_primes:
+        isPrime = True
+        counter += 1
+        for i in range(2, int(math.sqrt(counter)) + 1):
+            if(counter % i == 0):
+                isPrime = False
+        if isPrime:
+            list.append(counter)
+
+            
     return list
